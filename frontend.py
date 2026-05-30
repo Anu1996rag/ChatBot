8320febb2037022153547935db07bace0ba4a954
Anu1996rag <anuragpatils1996@gmail.com>
2026-05-30 18:19:49 +0530

    added streaming functionality

--- frontend.py
+++ frontend.py
@@ -18,8 +18,12 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    response = chatbot.invoke({"messages": [HumanMessage(content=user_input)]}, config=CONFIG)
-    ai_response = response["messages"][-1].content
-    st.session_state["message_history"].append({"role": "assistant", "content": ai_response})
     with st.chat_message('assistant'):
-        st.text(ai_response)
+        ai_response = st.write_stream(
+            message.content for message, metadata in chatbot.stream(
+                {"messages": [HumanMessage(content=user_input)]},
+                config=CONFIG,
+                stream_mode="messages"
+            )
+        )
+    st.session_state["message_history"].append({"role": "assistant", "content": ai_response})
